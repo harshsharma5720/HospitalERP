6c0613c2113ae8770f12424f5ee4b682aca0f3a5
harshsharma5720 <harshvardhan368152@gmail.com>
2025-09-10 15:18:26 +0530

    Updated AppointmentService and fixed Optional handling

--- hospitalERP/src/main/java/ITmonteur/example/hospitalERP/controller/AppointmentController.java
+++ hospitalERP/src/main/java/ITmonteur/example/hospitalERP/controller/AppointmentController.java
@@ -4,9 +4,7 @@ import ITmonteur.example.hospitalERP.dto.AppointmentDTO;
 import ITmonteur.example.hospitalERP.services.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,9 +15,53 @@ public class AppointmentController {
     @Autowired
     private AppointmentService appointmentService;
 
-    @GetMapping
+    @GetMapping("/getAll")
     public ResponseEntity<List<AppointmentDTO>> getAllAppointments(){
+        List<AppointmentDTO> appointmentDTOList = this.appointmentService.getAllAppointments();
+            return ResponseEntity.ok(appointmentDTOList);
+    }
 
+    @GetMapping("appointmentId/{myId}")
+    public ResponseEntity<AppointmentDTO> getAppointmentByAppointmentID(@PathVariable long myId){
+        AppointmentDTO appointmentDTO = this.appointmentService.getAppointmentByID(myId);
+        return  ResponseEntity.ok(appointmentDTO);
 
+    }
+
+    @PostMapping("/NewAppointment")
+    public ResponseEntity<String> createNewAppointment(@RequestBody AppointmentDTO appointmentDTO){
+        if(this.appointmentService.createAppointment(appointmentDTO)){
+            return ResponseEntity.ok("Your slot has been booked");
+        }
+        else {
+            return ResponseEntity.ok("Error occured");
+        }
+    }
+
+    @DeleteMapping("/CancelAppointment/{appointmentId}")
+    public ResponseEntity<String> deleteAppointmentByAppointmentId(@PathVariable long appointmentId){
+        if (this.appointmentService.deleteAppointmentByID(appointmentId)){
+            return ResponseEntity.ok("Your appointment slot has been deleted successfully!!");
+        }
+        else{
+            return ResponseEntity.ok("you entered the wrong credentials..");
+        }
+    }
+
+    @DeleteMapping("/cancelALlAppointments")
+    public ResponseEntity<String> deleteAllAppointment(){
+        if(this.appointmentService.deleteAllAppointments()){
+            return ResponseEntity.ok("Deleted all appointments successfully!!");
+        }
+        else{
+            return ResponseEntity.ok("Error occured..");
+        }
+    }
+
+    @PutMapping("/update/{appointmentId}")
+    public ResponseEntity<AppointmentDTO> updateAppointment(@PathVariable long appointmentId,
+                                                            @RequestBody AppointmentDTO appointmentDTO){
+        AppointmentDTO appointmentDTO1 = this.appointmentService.updateAppointmentById(appointmentId,appointmentDTO);
+        return ResponseEntity.ok(appointmentDTO1);
     }
 }
--- hospitalERP/src/main/java/ITmonteur/example/hospitalERP/services/AppointmentService.java
+++ hospitalERP/src/main/java/ITmonteur/example/hospitalERP/services/AppointmentService.java
@@ -20,6 +20,51 @@ public class AppointmentService {
 
     public List<AppointmentDTO> getAllAppointments(){
         List<Appointment> appointments = this.appointmentRepository.findAll();
+        List<AppointmentDTO> appointmentDTOList = appointments.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+        return appointmentDTOList;
 
+    }
+    public AppointmentDTO getAppointmentByID(long appointmentID ){
+        Appointment appointment = this.appointmentRepository.findById(appointmentID)
+                .orElseThrow(() -> new RuntimeException("Appointment not found with ID: " + appointmentID));
+        AppointmentDTO appointmentDTO = this.convertToDTO(appointment);
+        return appointmentDTO;
+    }
+    public boolean createAppointment(AppointmentDTO appointmentDTO){
+        Appointment appointment = this.convertToEntities(appointmentDTO);
+        this.appointmentRepository.save(appointment);
+        return true;
+    }
+    public boolean deleteAppointmentByID(long appointmentID){
+        this.appointmentRepository.deleteById(appointmentID);
+        return true;
+    }
+    public boolean deleteAllAppointments(){
+        this.appointmentRepository.deleteAll();
+        return true;
+    }
+    public AppointmentDTO updateAppointmentById(long appointmentID,AppointmentDTO appointmentDTO){
+        Appointment appointment = this.appointmentRepository.findById(appointmentID)
+                .orElseThrow(() -> new RuntimeException("Appointment not found with ID: " + appointmentID));
+        appointment.setAge(appointmentDTO.getAge());
+        appointment.setGender(appointmentDTO.getGender());
+        appointment.setPatientName(appointmentDTO.getPatientName());
+        appointment.setEmail(appointmentDTO.getEmail());
+        appointment.setDoctor(appointmentDTO.getDoctor());
+        appointment.setPhoneNo(appointmentDTO.getPhoneNo());
+        return convertToDTO(appointmentRepository.save(appointment));
+
+    }
+//    public AppointmentDTO getByDrName(String drName){
+//        AppointmentDTO appointmentDTO = this.appointmentRepository.findByDrName()
+//    }
+
+    private AppointmentDTO convertToDTO(Appointment appointment) {
+        return modelMapper.map(appointment, AppointmentDTO.class);
+    }
+    private Appointment convertToEntities(AppointmentDTO appointmentDTO){
+        return modelMapper.map(appointmentDTO,Appointment.class);
     }
 }
