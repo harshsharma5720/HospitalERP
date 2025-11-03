56bb894671f930059d2cbb8fd2a06aad4955b460
harshsharma5720 <harshvardhan368152@gmail.com>
2025-11-03 14:37:53 +0530

    Done some changes to resolve some errors occuring at the time of patient register

--- hospitalERP/src/main/java/ITmonteur/example/hospitalERP/entities/PtInfo.java
+++ hospitalERP/src/main/java/ITmonteur/example/hospitalERP/entities/PtInfo.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Table(name = "patient")
 public class PtInfo {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long patientId;
     private String patientName;
     @Column(unique = true , nullable = false)
